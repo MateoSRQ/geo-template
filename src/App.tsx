a52332f9fed66328b704ab5aa71a921277590a71
Administrator <sanroman_me@hotmail.com>
2020-09-26 23:25:23 -0500

    Initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from "react";
+import { setStatus, getStatus } from "./store/slices/app";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {}, []);
+  const status = useSelector(getStatus);
+
+  const handleClick = () => {
+    console.log("click");
+    dispatch(setStatus());
+    setStatus();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onClick={handleClick}>
+      {status}
     </div>
   );
 }
